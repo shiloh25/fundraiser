d826d5d348633991eda58504d1ac3e0f0a6e9a07
shiloh25 <165841775+shiloh25@users.noreply.github.com>
2024-05-01 09:41:59 +1200

    Add files via upload

--- 00_FR_Base_v1.py
+++ 00_FR_Base_v1.py
@@ -20,6 +20,24 @@ def num_check(question, error, num_type):
             print(error)
 
 
+# checks that user entered either yes or no
+def yes_no(question):
+
+    to_check = ['yes', 'no']
+
+    valid = False
+    while not valid:
+
+        response = input(question).lower()
+
+        for var_item in to_check:
+            if response == var_item:
+                return response
+            elif response == var_item[0]:
+                return var_item
+
+        print("Please enter either yes or no\n")
+
 # main routine goes here
 get_int = num_check("How many do you need? ",
                     "Please enter an amount more than 0\n", int)
